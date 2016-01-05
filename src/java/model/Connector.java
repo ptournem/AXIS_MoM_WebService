234a3a75c51c8f5b42adecc394998b2be93e1d58
loannguyen <loan.nguyen@ig2i.fr>
2016-01-05 08:39:53 +0100

    archi du fichier connector

---
+++ src/java/model/Connector.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.apache.jena.ontology.OntModel;
+
+/**
+ *
+ * @author loannguyen
+ */
+public class Connector {
+   
+    public static void main(String args[]) {
+        //test
+    }
+
+    static String s;
+
+    // Open a connection to MonFichierOwl.OWL
+    public static boolean loadModels(OntModel model) {
+
+        //OntModel mode = null;
+        return true;
+    }
+    
+    public static boolean executeQuery() {
+
+        //OntModel mode = null;	
+                
+//        Select
+//        dans notre BDD
+//        dans le LoD et dans notre BDD   
+//        Insert  (uniquement notre BDD)
+//        Update (uniquement notre BDD)
+//        Delete (uniquement notre BDD)
+
+        return true;
+    }
+    
+    public static boolean construct() {
+
+        // on construit un graph de resultats
+        
+        return true;
+
+    } 
+
+}
