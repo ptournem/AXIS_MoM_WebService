d8e98043a9bc4ed181993f32aecafa62e07a764e
Mélanie Bailleul <bailleul.melanie@gmail.com>
2016-01-07 12:11:18 +0100

    commit mel

--- src/java/model/Connector.java
+++ src/java/model/Connector.java
@@ -5,17 +5,21 @@
  */
 package model;
 
+import java.io.InputStream;
 import java.net.URI;
 import java.util.UUID;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateProcessor;
+import org.apache.jena.util.FileManager;
 
 /**
  *
@@ -26,14 +30,28 @@ public class Connector {
 
     public static void main(String args[]) {
         //test
-        System.out.println("main");
+        loadModels("/Users/Mélanie/Documents/AXIS_MoM_WebService/src/java/resources/axis-csrm-datamodel-MoM.owl");
     }
 
     
 
     public static Model loadModels(String url) { //mélanoche
         //todo
-        return null;
+     url = "/Users/Mélanie/Documents/AXIS_MoM_WebService/src/java/resources/axis-csrm-datamodel-MoM.owl";
+	
+	// Create an empty model
+	OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
+	
+	// Use the FileManager to find the input file
+	InputStream in = FileManager.get().open(url);
+
+	if (in == null)
+		throw new IllegalArgumentException("File: "+url+" not found");
+
+	// Read the RDF/XML file
+	model.read(in, null);
+       model.write(System.out);
+        return model;
     }
     
     public static boolean executeQuery(String str) {
