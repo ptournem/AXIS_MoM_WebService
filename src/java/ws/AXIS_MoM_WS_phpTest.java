38a1309f0477b642af92d2014ca1523a77125666
Paul Tournemaine <p.tournemaine@a2si-identit.fr>
2016-01-19 14:36:13 +0100

    TEST : ajout d'exemple plus parlant pour le WS de test PHP

--- src/java/ws/AXIS_MoM_WS_phpTest.java
+++ src/java/ws/AXIS_MoM_WS_phpTest.java
@@ -61,12 +61,34 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	p2.setName("Aime");
 	p2.setType("URI");
 	Entity e2 = new Entity();
-	e2.setImage("http://1.1.1.2/bmi/static.ladepeche.fr/content/media/image/zoom/2011/03/07/603056.jpg");
+	e2.setImage("http://t2.uccdn.com/fr/images/2/0/5/img_comment_ouvrir_une_canette_de_coca_secouee_6502_orig.jpg");
 	e2.setName("Coca");
-	e2.setType("Object");
+	e2.setType("object");
 	e2.setURI("coca");
 	p2.setEnt(e2);
 	list.add(p2);
+
+	Property p3 = new Property();
+	p3.setName("Est de nationalité");
+	p3.setType("URI");
+	Entity e3 = new Entity();
+	e3.setImage("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Flag_of_the_United_States_%28Pantone%29.svg/320px-Flag_of_the_United_States_%28Pantone%29.svg.png");
+	e3.setName("Américain");
+	e3.setType("organisation");
+	e3.setURI("USA");
+	p3.setEnt(e3);
+	list.add(p3);
+
+	Property p4 = new Property();
+	p4.setName("Conjoint");
+	p4.setType("URI");
+	Entity e4 = new Entity();
+	e4.setImage("https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Martin_Luther_King_Jr_NYWTS_5.jpg/440px-Martin_Luther_King_Jr_NYWTS_5.jpg");
+	e4.setName("Coretta Scott King");
+	e4.setType("person");
+	e4.setURI("CSK");
+	p4.setEnt(e4);
+	list.add(p4);
 
 	Property[] ret = new Property[list.size()];
 	return (Property[]) list.toArray(ret);
@@ -77,14 +99,14 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	ArrayList<Entity> list = new ArrayList<Entity>();
 
 	Entity e1 = new Entity();
-	e1.setImage("http://1.1.1.2/bmi/static.ladepeche.fr/content/media/image/zoom/2011/03/07/603056.jpg");
+	e1.setImage("http://t2.uccdn.com/fr/images/2/0/5/img_comment_ouvrir_une_canette_de_coca_secouee_6502_orig.jpg");
 	e1.setName("Canette de coca");
 	e1.setURI("coca");
 	e1.setType("object");
 	list.add(e1);
 
 	Entity e2 = new Entity();
-	e2.setImage("http://1.1.1.1/bmi/cp91279.biography.com/1000509261001/1000509261001_1891997649001_History-Bill-Clinton-on-MLK-SF.jpg");
+	e2.setImage("http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NTU2MzE2MjgwNDg5NDgz.jpg");
 	e2.setName("Martin Luther King");
 	e2.setURI("MLK");
 	e2.setType("person");
@@ -99,14 +121,14 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	ArrayList<Entity> list = new ArrayList<>();
 
 	Entity e1 = new Entity();
-	e1.setImage("http://1.1.1.2/bmi/static.ladepeche.fr/content/media/image/zoom/2011/03/07/603056.jpg");
+	e1.setImage("http://t2.uccdn.com/fr/images/2/0/5/img_comment_ouvrir_une_canette_de_coca_secouee_6502_orig.jpg");
 	e1.setName("Canette de coca");
 	e1.setURI("coca");
 	e1.setType("object");
 	list.add(e1);
 
 	Entity e2 = new Entity();
-	e2.setImage("http://1.1.1.1/bmi/cp91279.biography.com/1000509261001/1000509261001_1891997649001_History-Bill-Clinton-on-MLK-SF.jpg");
+	e2.setImage("http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NTU2MzE2MjgwNDg5NDgz.jpg");
 	e2.setName("Martin Luther King");
 	e2.setURI("MLK");
 	e2.setType("person");
@@ -138,15 +160,15 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	ArrayList<Comment> list = new ArrayList<>();
 
 	Comment c1 = new Comment();
-	c1.setAuthorName("Babibel");
-	c1.setEmail("babibel@gmail.com");
-	c1.setComment("Très belle oeuvre");
+	c1.setAuthorName("Babar");
+	c1.setEmail("babar@gmail.com");
+	c1.setComment("Une œuvre que j’ai vraiment apprécié regarder. Elle symbolise tellement pour moi et pour l’histoire de notre monde.");
 	list.add(c1);
 
 	Comment c2 = new Comment();
 	c2.setAuthorName("Babibel");
 	c2.setEmail("babibel@gmail.com");
-	c2.setComment("Très belle oeuvre");
+	c2.setComment("Bluffant, je n’ai jamais été aussi émue devant une œuvre. Et pourtant j’ai visité de nombreux musées tout au long de ma vie...");
 	list.add(c2);
 
 	Comment[] ret = new Comment[list.size()];
@@ -158,14 +180,14 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	ArrayList<Entity> list = new ArrayList<>();
 
 	Entity e1 = new Entity();
-	e1.setImage("http://1.1.1.2/bmi/static.ladepeche.fr/content/media/image/zoom/2011/03/07/603056.jpg");
+	e1.setImage("http://t2.uccdn.com/fr/images/2/0/5/img_comment_ouvrir_une_canette_de_coca_secouee_6502_orig.jpg");
 	e1.setName("Canette de coca");
 	e1.setURI("coca");
 	e1.setType("object");
 	list.add(e1);
 
 	Entity e2 = new Entity();
-	e2.setImage("http://1.1.1.1/bmi/cp91279.biography.com/1000509261001/1000509261001_1891997649001_History-Bill-Clinton-on-MLK-SF.jpg");
+	e2.setImage("http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NTU2MzE2MjgwNDg5NDgz.jpg");
 	e2.setName("Martin Luther King");
 	e2.setURI("MLK");
 	e2.setType("person");
@@ -186,20 +208,20 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	list.add(p1);
 
 	PropertyAdmin p2 = new PropertyAdmin();
-	p2.setName("Aime");
+	p2.setName("Apprécie");
 	p2.setType("URI");
 
 	Entity e2 = new Entity();
-	e2.setImage("http://1.1.1.2/bmi/static.ladepeche.fr/content/media/image/zoom/2011/03/07/603056.jpg");
+	e2.setImage("http://t2.uccdn.com/fr/images/2/0/5/img_comment_ouvrir_une_canette_de_coca_secouee_6502_orig.jpg");
 	e2.setName("Coca");
-	e2.setType("Object");
+	e2.setType("object");
 	e2.setURI("coca");
 	p2.setEntity_locale(e2);
 
 	Entity e1 = new Entity();
 	e1.setImage("http://www.sushitime-france.fr/wp-content/uploads/2015/01/canette-coca-33cl.jpg");
 	e1.setName("Coca Dbpedia");
-	e1.setType("Object");
+	e1.setType("object");
 	e1.setURI("coca");
 	p2.setEntity_dbpedia(e1);
 	list.add(p2);
@@ -214,7 +236,7 @@ public class AXIS_MoM_WS_phpTest implements AXIS_MoM_WSInterface {
 	    return null;
 	}
 
-	e.setImage("http://1.1.1.1/bmi/cp91279.biography.com/1000509261001/1000509261001_1891997649001_History-Bill-Clinton-on-MLK-SF.jpg");
+	e.setImage("http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NTU2MzE2MjgwNDg5NDgz.jpg");
 	e.setName("Martin Luther King");
 	e.setType("person");
 
