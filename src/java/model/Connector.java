fac7d2efb35f055f18043a040825664a09772e69
Loan Nguyen <loannguyen@MacBook-Air-de-Loan.local>
2016-01-19 19:30:32 +0100

    ADD : insertPlaceOfBirth ok dans Person EDIT : restructuration fichier de Test pour plus de visibilité

--- src/java/model/Connector.java
+++ src/java/model/Connector.java
@@ -358,6 +358,7 @@ public class Connector {
                 + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
                 + "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
                 + "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
+                + "PREFIX schema: <https://schema.org/>"
                 + "INSERT DATA { "
                 + "<%s>"
                 + " %s "
